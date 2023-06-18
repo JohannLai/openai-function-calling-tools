268f2bac2e9ff1212816128b021d360628ebd881
Johannlai <johann_lai@outlook.com>
2023-06-18 15:29:42 +0800

    :recycle: Refactor isNode.ts for improved functionality.

--- utils/isNode.ts
+++ utils/isNode.ts
@@ -4,25 +4,3 @@ export const isNode = () => {
   typeof process.versions !== "undefined" &&
   typeof process.versions.node !== "undefined"
 }
-
-export const getEnv = () => {
-  let env: string;
-  if (isBrowser()) {
-    env = "browser";
-  } else if (isNode()) {
-    env = "node";
-  } else if (isWebWorker()) {
-    env = "webworker";
-  } else if (isJsDom()) {
-    env = "jsdom";
-  } else if (isDeno()) {
-    env = "deno";
-  } else {
-    env = "other";
-  }
-
-  return env;
-};
-
-export const isBrowser = () =>
-  typeof window !== "undefined" && typeof window.document !== "undefined";
