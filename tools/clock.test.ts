8a2d4f2242d9ebbad5a24e99d70d54f40fd55855
johannlai <johann_lai@outlook.com>
2023-06-21 19:43:52 +0800

    :test_tube: add codecov
    add codecov

--- tools/clock.test.ts
+++ tools/clock.test.ts
@@ -4,7 +4,14 @@ import moment from 'moment-timezone';
 
 it('should return current time', () => {
   const expectedTime = moment().format('YYYY-MM-DD HH:mm:ss Z');
-  const {clock} = new Clock();
+  const { clock } = new Clock();
 
   expect(clock()).toEqual(expectedTime);
+});
+
+it('should return current time in a given timezone', () => {
+  const expectedTime = moment().tz('America/New_York').format('YYYY-MM-DD HH:mm:ss Z');
+  const { clock } = new Clock();
+
+  expect(clock('America/New_York')).toEqual(expectedTime);
 });
