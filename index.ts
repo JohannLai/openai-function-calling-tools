82cb106db740d68aeac59fcb0c81ec205d4dc00d
johannlai <johann_lai@outlook.com>
2023-06-27 17:45:05 +0800

    :bug: remove vm2 for javascroptInerpreter

--- index.ts
+++ index.ts
@@ -3,6 +3,6 @@ export { createGoogleCustomSearch } from './tools/googleCustomSearch';
 export { createClock } from './tools/clock';
 export { createWebBrowser } from './tools/webbrowser';
 export { createReadFileTool, createWriteFileTool } from './tools/fs';
-export { createJavaScriptInterpreter } from './tools/javaScriptInterpreter';
+// export { createJavaScriptInterpreter } from './tools/javaScriptInterpreter';
 export { createAIPlugin } from './tools/aiplugin';
 export { createRequest } from './tools/request';
