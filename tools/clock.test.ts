c47d2d1d4af174671cb5ff49ef09afc10c59b4b6
johannlai <johann_lai@outlook.com>
2023-06-30 15:35:17 +0800

    :bug: change ts type

--- tools/clock.test.ts
+++ tools/clock.test.ts
@@ -4,14 +4,14 @@ import moment from 'moment-timezone';
 
 it('should return current time', () => {
   const expectedTime = moment().format('YYYY-MM-DD HH:mm:ss Z');
-  const { clock } = createClock();
+  const [clock] = createClock();
 
   expect(clock({})).toEqual(expectedTime);
 });
 
 it('should return current time in a given timezone', () => {
   const expectedTime = moment().tz('America/New_York').format('YYYY-MM-DD HH:mm:ss Z');
-  const { clock } = createClock();
+  const [clock] = createClock();
 
   expect(clock({
     timeZone: 'America/New_York',
--- tools/tool.ts
+++ tools/tool.ts
@@ -1,39 +1,39 @@
 import { z } from "zod";
 import { zodToJsonSchema } from "zod-to-json-schema";
-import { VM } from "vm2";
 
-export interface ToolInterface {
-  [name: string]: (params: any) => any | object;
-}
+export type ToolInterface<P extends z.ZodType<any, any>, R extends z.ZodType<any, any>> = [
+  (params: z.infer<P>) => z.infer<R>,
+  {
+    name: string;
+    description: string;
+    parameters: object;
+  }
+];
 
-class Tool {
-  paramsSchema: z.ZodObject<any>;
+class Tool<P extends z.ZodType<any, any>, R extends z.ZodType<any, any>> {
+  paramsSchema: P;
   name: string;
   description: string;
-  execute: (params: any) => any;
-  tool: ToolInterface
+  execute: (params: z.infer<P>) => z.infer<R>;
+  tool: ToolInterface<P, R>
 
-  constructor(paramsSchema: z.ZodObject<any>, name: string, description: string, execute: (params: any) => any) {
+  constructor(paramsSchema: P, name: string, description: string, execute: (params: z.infer<P>) => z.infer<R>) {
     this.paramsSchema = paramsSchema;
     this.name = name;
     this.description = description;
     this.execute = execute;
 
-    this.tool = {
-      [name]: this.run.bind(this),
-      [`${name}Schema`]: this.getSchema(),
-    }
+    this.tool = [
+      this.run.bind(this),
+      {
+        name: this.name,
+        description: this.description,
+        parameters: zodToJsonSchema(this.paramsSchema),
+      }
+    ];
   }
 
-  getSchema() {
-    return {
-      name: this.name,
-      description: this.description,
-      parameters: zodToJsonSchema(this.paramsSchema),
-    };
-  }
-
-  run(params: any) {
+  run(params: z.infer<P>): z.infer<R> {
     try {
       const validatedParams = this.paramsSchema.parse(params);
       return this.execute(validatedParams);
