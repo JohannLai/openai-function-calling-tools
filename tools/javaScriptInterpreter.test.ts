30673f1908dfb585a26fcfda80d30fec2eeb1330
johannlai <johann_lai@outlook.com>
2023-10-09 10:48:31 +0800

    fix: Temporary blocking of vm2 of js interpreter
    close #12

--- tools/javaScriptInterpreter.test.ts
+++ tools/javaScriptInterpreter.test.ts
@@ -1,34 +1,37 @@
-import { createJavaScriptInterpreter } from "./javaScriptInterpreter";
+// import { createJavaScriptInterpreter } from "./javaScriptInterpreter";
 import { expect, it } from 'vitest';
-
 it('should interpret JavaScript code correctly', () => {
-  const [javaScriptInterpreter] = createJavaScriptInterpreter();
-  const result = javaScriptInterpreter({
-    code: `
-    1 + 1
-    `,
-  });
-  expect(result).toBe(2);
+  expect(2).toBe(2);
 });
 
-it('should timeout for long running scripts', () => {
-  const [javaScriptInterpreter] = createJavaScriptInterpreter();
-  const result = javaScriptInterpreter({
-    code: `
-    while (true) {}
-    `,
-  });
+// it('should interpret JavaScript code correctly', () => {
+//   const [javaScriptInterpreter] = createJavaScriptInterpreter();
+//   const result = javaScriptInterpreter({
+//     code: `
+//     1 + 1
+//     `,
+//   });
+//   expect(result).toBe(2);
+// });
 
-  expect(result).toBe("Failed to execute script: Script execution timed out after 5000ms")
-});
+// it('should timeout for long running scripts', () => {
+//   const [javaScriptInterpreter] = createJavaScriptInterpreter();
+//   const result = javaScriptInterpreter({
+//     code: `
+//     while (true) {}
+//     `,
+//   });
 
-it('should not have access to Node.js environment', () => {
-  const [javaScriptInterpreter] = createJavaScriptInterpreter();
-  const result = javaScriptInterpreter({
-    code: `
-    process.exit(1)
-    `,
-  });
+//   expect(result).toBe("Failed to execute script: Script execution timed out after 5000ms")
+// });
 
-  expect(result).toBe("Failed to execute script: process is not defined")
-});
+// it('should not have access to Node.js environment', () => {
+//   const [javaScriptInterpreter] = createJavaScriptInterpreter();
+//   const result = javaScriptInterpreter({
+//     code: `
+//     process.exit(1)
+//     `,
+//   });
+
+//   expect(result).toBe("Failed to execute script: process is not defined")
+// });
--- tools/javaScriptInterpreter.ts
+++ tools/javaScriptInterpreter.ts
@@ -1,28 +1,28 @@
-import { z } from "zod";
-import { VM } from "vm2";
-import { Tool } from "./tool";
+// import { z } from "zod";
+// import { VM } from "vm2";
+// import { Tool } from "./tool";
 
-function createJavaScriptInterpreter() {
-  const paramsSchema = z.object({
-    code: z.string(),
-  });
-  const name = "javaScriptInterpreter";
-  const description = "Useful for running JavaScript code in sandbox. Input is a string of JavaScript code, output is the result of the code.";
+// function createJavaScriptInterpreter() {
+//   const paramsSchema = z.object({
+//     code: z.string(),
+//   });
+//   const name = "javaScriptInterpreter";
+//   const description = "Useful for running JavaScript code in sandbox. Input is a string of JavaScript code, output is the result of the code.";
 
-  const execute = (params: z.infer<typeof paramsSchema>) => {
-    const { code } = params;
-    const vm = new VM({
-      timeout: 5000,
-      sandbox: {},
-    });
-    try {
-      return vm.run(code);
-    } catch (error) {
-      return `Failed to execute script: ${error.message}`;
-    }
-  };
+//   const execute = (params: z.infer<typeof paramsSchema>) => {
+//     const { code } = params;
+//     const vm = new VM({
+//       timeout: 5000,
+//       sandbox: {},
+//     });
+//     try {
+//       return vm.run(code);
+//     } catch (error) {
+//       return `Failed to execute script: ${error.message}`;
+//     }
+//   };
 
-  return new Tool<typeof paramsSchema, z.ZodType<any, any>>(paramsSchema, name, description, execute).tool;
-}
+//   return new Tool<typeof paramsSchema, z.ZodType<any, any>>(paramsSchema, name, description, execute).tool;
+// }
 
-export { createJavaScriptInterpreter };
+// export { createJavaScriptInterpreter };
