f9bf0f0f1168093632373335fe711970024ddf8c
johannlai <johann_lai@outlook.com>
2024-01-12 19:56:35 +0800

    :bug: export createSerpApiCustomSearch

--- index.ts
+++ index.ts
@@ -1,5 +1,8 @@
 export { createCalculator } from './tools/calculator';
 export { createGoogleCustomSearch } from './tools/googleCustomSearch';
+export { createBingCustomSearch } from './tools/bingCustomSearch';
+export { createSerpApiCustomSearch } from './tools/serpApiCustomSearch';
+export { createSerperCustomSearch } from './tools/serperCustomSearch';
 export { createClock } from './tools/clock';
 export { createWebBrowser } from './tools/webbrowser';
 export { createReadFileTool, createWriteFileTool } from './tools/fs';
